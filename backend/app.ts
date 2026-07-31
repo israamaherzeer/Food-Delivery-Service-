ae4bc96e889ab614ffa2a95b0ab318629fee5bf9
ISRAA <211106@ppu.edu.ps>
2026-07-31 14:47:37 +0300

    update

--- backend/app.ts
+++ backend/app.ts
@@ -23,7 +23,7 @@ const app = express();
 const PORT = Number(process.env.PORT) || 5000;
 
 app.use(cors({
-  origin: "https://e31d3026.food-delivery-service2.pages.dev",
+  origin: true,
   credentials: true,
 }));
 app.use(express.json());
